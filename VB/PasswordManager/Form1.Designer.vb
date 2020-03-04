37587345778a8c7276fb9f8629f485963c55679c
347jhjj347 <avdruslan@mail.ru>
2020-03-04 20:58:16 +0500

    O.12 v1 alpha
    Добавлены типы данных

--- VB/PasswordManager/Form1.Designer.vb
+++ VB/PasswordManager/Form1.Designer.vb
@@ -33,48 +33,57 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ComboBox1
         '
-        Me.ComboBox1.BackColor = System.Drawing.Color.Silver
+        Me.ComboBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(384, 210)
+        Me.ComboBox1.Location = New System.Drawing.Point(524, 69)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 0
         '
         'TextBox1
         '
-        Me.TextBox1.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.TextBox1.Location = New System.Drawing.Point(15, 273)
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TextBox1.Location = New System.Drawing.Point(12, 160)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(264, 31)
         Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Visible = False
         '
         'Button1
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Button1.Location = New System.Drawing.Point(15, 387)
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Location = New System.Drawing.Point(12, 387)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(264, 40)
         Me.Button1.TabIndex = 6
         Me.Button1.Text = "Записать данные "
         Me.Button1.UseVisualStyleBackColor = False
+        Me.Button1.Visible = False
         '
         'TextBox2
         '
-        Me.TextBox2.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.TextBox2.Location = New System.Drawing.Point(15, 332)
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TextBox2.Location = New System.Drawing.Point(12, 240)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(264, 31)
         Me.TextBox2.TabIndex = 7
+        Me.TextBox2.Visible = False
         '
         'TextBox3
         '
-        Me.TextBox3.BackColor = System.Drawing.Color.Silver
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox3.Location = New System.Drawing.Point(527, 210)
         Me.TextBox3.Multiline = True
         Me.TextBox3.Name = "TextBox3"
@@ -83,7 +92,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Button2.Location = New System.Drawing.Point(524, 387)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(264, 40)
@@ -94,17 +103,16 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.BackColor = System.Drawing.Color.Silver
-        Me.Label4.Location = New System.Drawing.Point(315, 213)
+        Me.Label4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(393, 194)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
         Me.Label4.TabIndex = 12
-        Me.Label4.Text = "Label4"
         '
         'TextBox4
         '
-        Me.TextBox4.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.TextBox4.Location = New System.Drawing.Point(15, 210)
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TextBox4.Location = New System.Drawing.Point(12, 32)
         Me.TextBox4.Multiline = True
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(264, 24)
@@ -113,8 +121,8 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Label6.Location = New System.Drawing.Point(24, 194)
+        Me.Label6.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label6.Location = New System.Drawing.Point(12, 16)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(57, 13)
         Me.Label6.TabIndex = 15
@@ -123,22 +131,88 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Label7.Location = New System.Drawing.Point(24, 316)
+        Me.Label7.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label7.Location = New System.Drawing.Point(20, 210)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(45, 13)
         Me.Label7.TabIndex = 16
         Me.Label7.Text = "Пароль"
+        Me.Label7.Visible = False
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Label8.Location = New System.Drawing.Point(24, 247)
+        Me.Label8.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label8.Location = New System.Drawing.Point(17, 129)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(41, 13)
         Me.Label8.TabIndex = 17
         Me.Label8.Text = "Логин "
+        Me.Label8.Visible = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(314, 174)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(112, 17)
+        Me.RadioButton1.TabIndex = 20
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Логины и пароли"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(314, 210)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(111, 17)
+        Me.RadioButton2.TabIndex = 21
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Простые данные"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TextBox5.Location = New System.Drawing.Point(12, 160)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(264, 212)
+        Me.TextBox5.TabIndex = 22
+        Me.TextBox5.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(17, 129)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Данные"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(311, 151)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 13)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Тип данных для записи"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button3.Location = New System.Drawing.Point(12, 387)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(264, 40)
+        Me.Button3.TabIndex = 25
+        Me.Button3.Text = "Записать данные "
+        Me.Button3.UseVisualStyleBackColor = False
+        Me.Button3.Visible = False
         '
         'Form1
         '
@@ -146,6 +220,12 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Azure
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
@@ -175,4 +255,10 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
 End Class
--- VB/PasswordManager/Form1.vb
+++ VB/PasswordManager/Form1.vb
@@ -54,6 +54,72 @@ Public Class Form1
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
 
     End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        TextBox1.Visible = False
+        TextBox2.Visible = False
+        TextBox5.Visible = True
+        Label1.Visible = True
+        Label7.Visible = False
+        Label8.Visible = False
+        Button3.Visible = True
+        Button1.Visible = False
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox1.Visible = True
+        TextBox2.Visible = True
+        TextBox5.Visible = False
+        Label1.Visible = False
+        Label7.Visible = True
+        Label8.Visible = True
+        Button3.Visible = False
+        Button1.Visible = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            n = n + 1
+            ComboBox1.Items.Add(TextBox4.Text)
+            fw = New StreamWriter("F:\GitKraken\PasswordManager\PasswordManager\VB\PasswordManager\profiles\Index.txt", True)
+            fw.WriteLine(TextBox4.Text)
+            fw.Close()
+            fw = New StreamWriter("F:\GitKraken\PasswordManager\PasswordManager\VB\PasswordManager\date\" + (TextBox4.Text) + ".txt", True)
+            fw.WriteLine(TextBox5.Text)
+        Finally
+            'Очистить поле
+            TextBox4.Text = ""
+            'Очистить поле
+            TextBox5.Text = ""
+            'Закрыть файл.
+            fw.Close()
+        End Try
+    End Sub
 End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'For Each Item As ComboBox In Me.Controls.OfType(Of ComboBox)
+'Item.Items.Clear()
+'Item.Text = Nothing
+'Next
+'IO.File.WriteAllText("F:\GitKraken\PasswordManager\PasswordManager\VB\PasswordManager\profiles\Index.txt", "")
+'Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+'IO.File.WriteAllText("F:\GitKraken\PasswordManager\PasswordManager\VB\PasswordManager\profiles\Index.txt", "")
+
+'End Sub
+
 
 
